170d4595a0f6653428baeeb6a64492156740a245
Karunasri4 <karunasrigudikandhula@gmail.com>
2025-07-15 15:55:02 +0530

    Create AbstractionUsingInterface.java

---
+++ AbstractionUsingInterface.java
@@ -0,0 +1,19 @@
+interface Vehicle {
+    void start();  
+
+}
+
+
+class Car implements Vehicle {
+    public void start() {
+        System.out.println("Car is starting...");
+    }
+}
+
+
+public class AbstractionUsingInterface {
+    public static void main(String[] args) {
+        Vehicle car = new Car();  
+        car.start();              
+    }
+}
