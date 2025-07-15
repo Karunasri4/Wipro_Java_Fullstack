a945db77baaf016f68b23530cda3a6f4e8393ee6
Karunasri4 <karunasrigudikandhula@gmail.com>
2025-07-15 09:53:39 +0530

    Add files via upload

---
+++ CommandLine2.java
@@ -0,0 +1,10 @@
+   import java.util.*;
+public class CommandLine2 {
+     public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        String n1=sc.next();
+        System.out.println("Welcome"+" "+n1);
+    }
+}
+
+
