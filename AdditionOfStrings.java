446713dd5e5c5c6382eea5a917370bcb922d5c51
Karunasri4 <karunasrigudikandhula@gmail.com>
2025-07-15 09:54:46 +0530

    Add files via upload

---
+++ AdditionOfStrings.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class AdditionOfStrings {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        String n1=sc.next();
+        String n2=sc.next();
+        System.out.println(n1+" "+"Technologies"+" "+n2);
+    }
+}
