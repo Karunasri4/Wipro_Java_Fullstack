18e685636ac629835539ac2495b0c9260630ad63
Karunasri4 <karunasrigudikandhula@gmail.com>
2025-07-17 12:15:36 +0530

    Create ExceptionHandlingEx.java

---
+++ ExceptionHandlingEx.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class ExceptionHandlingEx {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the value a: ");
+        try{
+            int a=sc.nextInt();
+            System.out.println(a);
+        }
+        catch(InputMismatchException e){
+            System.out.println("pass integer ");
+        }
+    }
+}
