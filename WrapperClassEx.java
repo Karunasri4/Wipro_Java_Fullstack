462b0d0122f139e30bc3727d31a58f16cedb91a6
Karunasri4 <karunasrigudikandhula@gmail.com>
2025-07-18 11:38:31 +0530

    Create WrapperClassEx.java

---
+++ WrapperClassEx.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+
+public class WrapperClassEx {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("integer max val: "+Integer.MAX_VALUE);
+        System.out.println("integer min value: "+Integer.MIN_VALUE);
+        System.out.println("Double max val: "+Double.MAX_VALUE);
+        System.out.println("Double min value: "+Double.MIN_VALUE);
+        System.out.println("Long max val: "+Long.MAX_VALUE);
+        System.out.println("Long min value: "+Long.MIN_VALUE);
+        System.out.println("Short max val: "+Short.MAX_VALUE);
+        System.out.println("Short min value: "+Short.MIN_VALUE);
+        System.out.println("Byte max val: "+Byte.MAX_VALUE);
+        System.out.println("Byte min value: "+Byte.MIN_VALUE);
+        System.out.println("Float max val: "+Float.MAX_VALUE);
+        System.out.println("Float min value: "+Float.MIN_VALUE);
+
+       
+
+    }
+}
